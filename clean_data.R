a2f2e1bdc64f9091f63b86b692b96c84e0690864
Aksel Danielsen <akselsd@stud.ntnu.no>
2019-10-29 10:36:54 -0400

    Added script for cleaning data

---
+++ clean_data.R
@@ -0,0 +1,14 @@
+clean_data <- function(year) {
+  infile = paste(toString(year), ".csv", sep="")
+  data = read.csv(infile)
+  
+  data_boston = data[which(data$ORIGIN == "BOS" | data$DEST == "BOS"),]
+  
+  outfile = paste(toString(year), "_boston.csv", sep="")
+  write.csv(data_boston, outfile)
+}
+
+for (year in seq(2009, 2018)) {
+  print(paste("Cleaning year", year))
+  clean_data(year)
+}
