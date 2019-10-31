fab30c54096e3293e479727351548dfe2604c6c0
Aksel Danielsen <akselsd@stud.ntnu.no>
2019-10-31 11:17:06 -0400

    Added plots for outgoing departure and arrival delay

--- destinations.R
+++ destinations.R
@@ -27,3 +27,40 @@ plot = ggplot(df, aes(x=reorder(grouped, -Freq), Freq))+
 ggsave("Plots/destination_distribution_outgoing_2018.pdf")
 
 ## === End barplot ===
+
+outgoing = test[test$DEST != "BOS",]
+outgoing$DEP_DELAY = replace_na(outgoing$DEP_DELAY, 0)
+
+avg.dest.delay = aggregate(outgoing$DEP_DELAY, by=list(outgoing$DEST), FUN=mean)
+
+plot = ggplot(avg.dest.delay, aes(x=Group.1, x))+
+  geom_bar(stat="identity", width=1) +
+  theme(axis.text.x = element_text(angle = 90, size = 6)) +
+  theme(axis.ticks.x = element_blank()) +
+  xlab("Airport") +
+  ylab("Average departure delay in 2018") +
+  labs(title = "Departure delay out of Boston in 2018")
+
+ggsave("Plots/departure_delay_outgoing_2018.pdf")
+
+
+outgoing$ARR_DELAY = replace_na(outgoing$ARR_DELAY, 0)
+
+avg.arr.delay = aggregate(outgoing$ARR_DELAY, by=list(outgoing$DEST), FUN=mean)
+
+plot = ggplot(avg.arr.delay, aes(x=Group.1, x))+
+  geom_bar(stat="identity", width=1) +
+  theme(axis.text.x = element_text(angle = 90, size = 6)) +
+  theme(axis.ticks.x = element_blank()) +
+  xlab("Airport") +
+  ylab("Average arrival delay in 2018") +
+  labs(title = "Arrival delay of flights comming out of Boston in 2018")
+
+ggsave("Plots/departure_delay_outgoing_2018.pdf")
+
+
+
+
+
+
+
