8783dbc6d4f6af5142b9562fdf9b06b4619b099d
Aksel Danielsen <akselsd@stud.ntnu.no>
2019-11-05 11:17:50 -0500

    Clean cancelled and diverted flights before loading

--- load_data.R
+++ load_data.R
@@ -5,7 +5,24 @@
 # train = load_data(2013, 2017)   # Training data from 2013 to 2017
 # test = load_data(2018, 2018)    # Testing data, 2018 only
 #
+# Needs package testit, to install run install.packages("testit")
+#
+library(testit)
+
 load_data <- function(startyear, endyear) {
+  df = load_data_w_cancelled(startyear, endyear)
+  df = df[df$CANCELLED == 0,]
+  
+  assert("No NA in arrival delay column", nrow(df[is.na(df$ARR_DELAY),]) == 0)
+  assert("No NA in departure delay column", nrow(df[is.na(df$DEP_DELAY),]) == 0)
+  assert("No cancelled flights", nrow(df[df$CANCELLED == 1,]) == 0)
+  assert("No diverted flights", nrow(df[df$DIVERTED == 1,]) == 0)
+  
+  
+  return(df)
+}
+
+load_data_cancelled <- function(startyear, endyear) {
   df = data.frame()
   
   for (year in seq(startyear, endyear)) {
@@ -13,6 +30,11 @@ load_data <- function(startyear, endyear) {
     data = read.csv(infile)
     df = rbind(df, data)
   }
+  
+  df = df[df$DIVERTED == 0,]
+  
+  df$ARR_DELAY[df$ARR_TIME == df$CRS_ARR_TIME] <- 0
+  df$DEP_DELAY[df$DEP_TIME == df$CRS_DEP_TIME] <- 0
   
   return(df)
 }
