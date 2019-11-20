ff4568b33b27cc890734c378386f13e795bf7594
Aksel Danielsen <akselsd@stud.ntnu.no>
2019-11-20 14:27:06 -0500

    Fixed loader

--- load_data.R
+++ load_data.R
@@ -14,12 +14,14 @@ load_data <- function(startyear, endyear) {
   df = load_data_cancelled(startyear, endyear)
   df = df[df$CANCELLED == 0,]
   
+  drops = c("CANCELLED", "CANCELLATION_CODE")
+  
+  df = df[, !(names(df) %in% drops)]
+  
   assert("No NA in arrival delay column", nrow(df[is.na(df$ARR_DELAY),]) == 0)
   assert("No NA in departure delay column", nrow(df[is.na(df$DEP_DELAY),]) == 0)
   assert("No cancelled flights", nrow(df[df$CANCELLED == 1,]) == 0)
   assert("No diverted flights", nrow(df[df$DIVERTED == 1,]) == 0)
-  
-  
   
   return(df)
 }
@@ -29,6 +31,8 @@ load_data <- function(startyear, endyear) {
 #and is used in the main function load_data
 
 load_data_cancelled <- function(startyear, endyear) {
+  
+  assert("Endyear is greater than or equal to startyear", endyear >= startyear)
   df = data.frame()
   
   for (year in seq(startyear, endyear)) {
@@ -38,6 +42,10 @@ load_data_cancelled <- function(startyear, endyear) {
   }
   
   df = df[df$DIVERTED == 0,]
+  
+  drops = c("DIVERTED", "Unnamed..27", "X")
+  
+  df = df[, !(names(df) %in% drops)]
   
   df$ARR_DELAY[df$ARR_TIME == df$CRS_ARR_TIME] <- 0
   df$DEP_DELAY[df$DEP_TIME == df$CRS_DEP_TIME] <- 0
