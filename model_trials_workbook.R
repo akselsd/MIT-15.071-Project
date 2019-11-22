20ad2c46aaa798148cb931d4312bb51cade10a38
Jay-mk <jayanth.911@gmail.com>
2019-11-22 10:57:33 -0500

    yes

--- model_trials_workbook.R
+++ model_trials_workbook.R
@@ -9,8 +9,8 @@ summary(train)
 summary(test)
 
 
-
-tesmodel = glm( ISDELAYED~ + TIME_OF_DAY + AIR_TIME + DISTANCE, data=train, family=binomial)
+#LOGISTIC MODEL TRIAL WITH NO CONFLICTING VARIABLES BETWEEN TEST AND TRIAL
+tesmodel = glm( ISDELAYED~  SEASON + TIME_OF_DAY + AIR_TIME + DISTANCE, data=train, family=binomial)
 summary(model)
 
 pred =predict.glm(tesmodel,test)
@@ -21,7 +21,13 @@ confusion.matrix = table(test$ISDELAYED,threshold)
 accuracy = sum(diag(confusion.matrix)) / sum(confusion.matrix)
 accuracy
 
-summary(pred)
+
+#data types that can be accessed in reality
+
+train_future = subset(train, select = c(FL_DATE + SEASON + Weekday + TIME_OF_DAY + ORIGIN + DEST + OP_CARRIER + OP_CARRIER_FL_NUM + CRS_DEP_TIME + CRS_ARR_TIME + CRS_ELAPSED_TIME + DISTANCE))
+
+ summary(pred)
 
 
-findfactors = lm(train$ARR_DELAY ~ ., data = train)
+findfactors = lm(ARR_DELAY ~ . - FL_DATE, data = train)
+summary(findfactors)
