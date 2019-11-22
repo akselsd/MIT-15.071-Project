601eb049026572d063f6725a40418dea8ef9f761
Jay-mk <jayanth.911@gmail.com>
2019-11-22 10:22:52 -0500

    yes

--- model_trials_workbook.R
+++ model_trials_workbook.R
@@ -3,16 +3,25 @@ source("load_data.R")
 source("utils.R")
 
 
-train <- load_data(2009,2017)
+train <- load_data(2009,2016)
+test <- load_data(2017,2018)
 summary(train)
+summary(test)
 
 
 
-model = glm( ISDELAYED~ OP_CARRIER + ORIGIN + DEST + TIME_OF_DAY + season + AIR_TIME + DISTANCE, data=train2, family=binomial)
+tesmodel = glm( ISDELAYED~ + TIME_OF_DAY + AIR_TIME + DISTANCE, data=train, family=binomial)
 summary(model)
 
-test = load_data(2010,2010)
-test = seasonal_data(test)
-test = add_timeofday_column(test)
-test = add_binomial(test,15)
-pred =predict.glm(model,test)
+pred =predict.glm(tesmodel,test)
+threshold <- pred > .1
+table(threshold)
+table(test$ISDELAYED)
+confusion.matrix = table(test$ISDELAYED,threshold)
+accuracy = sum(diag(confusion.matrix)) / sum(confusion.matrix)
+accuracy
+
+summary(pred)
+
+
+findfactors = lm(train$ARR_DELAY ~ ., data = train)
