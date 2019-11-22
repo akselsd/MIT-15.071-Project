45f17d536c4b8e625839c1cc48dc8a3c4ef2420f
Phillip Schmedeman <phillipschmedeman@dhcp-18-20-156-3.dyn.mit.edu>
2019-11-22 10:42:30 -0500

    corrected weekday error in load data

--- load_data.R
+++ load_data.R
@@ -53,7 +53,6 @@ load_data_cancelled <- function(startyear, endyear) {
   df = add_timeofday_column(df)
   df = add_seasonal_data(df)
   df = add_binomial(df, 15)
-  df$WEEKDAY = weekdays(as.Date(df$FL_DATE,'%Y-%m-%d'))
-  df$WEEKDAY <- factor(flight$Weekday)
+  df$WEEKDAY = factor(weekdays(as.Date(df$FL_DATE,'%Y-%m-%d')))
   return(df)
 }
